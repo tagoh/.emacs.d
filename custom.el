212c745c40d5f3151c7ebff4f00efdd84c17e131
Akira TAGOH <akira@tagoh.org>
2025-08-29 22:39:02 +0900

    Update custom.el

--- custom.el
+++ custom.el
@@ -22,8 +22,8 @@
 ;; (setq centaur-tree-sitter nil)                 ; Enable tree-sitter or not: t or nil. Only available in 29+.
 ;; (setq centaur-chinese-calendar t)              ; Support Chinese calendar or not: t or nil
 ;; (setq centaur-player t)                        ; Enable players or not: t or nil
-;; (setq centaur-prettify-symbols-alist nil)      ; Alist of symbol prettifications. Nil to use font supports ligatures.
-;; (setq centaur-prettify-org-symbols-alist nil)  ; Alist of symbol prettifications for `org-mode'
+(setq centaur-prettify-symbols-alist nil)      ; Alist of symbol prettifications. Nil to use font supports ligatures.
+(setq centaur-prettify-org-symbols-alist nil)  ; Alist of symbol prettifications for `org-mode'
 
 ;; For Emacs devel
 ;; (setq package-user-dir (locate-user-emacs-file (format "elpa-%s" emacs-major-version)))
@@ -48,10 +48,11 @@ for the image type)."
   "Setup fonts."
   (when (display-graphic-p)
     ;; Set default font
-    (cl-loop for font in '("Cascadia Code" "Fira Code" "Jetbrains Mono"
-                           "SF Mono" "Hack" "Source Code Pro" "Menlo"
+    (cl-loop for font in '("FiraCode Nerd Font" "CaskaydiaCove Nerd Font"
+                           "Fira Code" "Cascadia Code" "Jetbrains Mono"
+                           "SF Mono" "Menlo" "Hack" "Source Code Pro"
                            "Monaco" "DejaVu Sans Mono" "Consolas")
-             when (font-installed-p font)
+             when (font-available-p font)
              return (set-face-attribute 'default nil
                                         :family font
                                         :height (cond (sys/macp 130)
@@ -59,8 +60,8 @@ for the image type)."
                                                       (t 100))))
 
     ;; Set mode-line font
-    ;; (cl-loop for font in '("Menlo" "SF Pro Display" "Helvetica")
-    ;;          when (font-installed-p font)
+    ;; (cl-loop for font in '("SF Mono" "Menlo" "SF Pro Display" "Helvetica")
+    ;;          when (font-available-p font)
     ;;          return (progn
     ;;                   (set-face-attribute 'mode-line nil :family font :height 120)
     ;;                   (when (facep 'mode-line-active)
@@ -69,23 +70,22 @@ for the image type)."
 
     ;; Specify font for all unicode characters
     (cl-loop for font in '("Apple Symbols" "Segoe UI Symbol" "Symbola" "Symbol")
-             when (font-installed-p font)
+             when (font-available-p font)
              return (set-fontset-font t 'symbol (font-spec :family font) nil 'prepend))
 
     ;; Emoji
     (cl-loop for font in '("Noto Color Emoji" "Apple Color Emoji" "Segoe UI Emoji")
-             when (font-installed-p font)
-             return (set-fontset-font t
-                                      (if (< emacs-major-version 28)'symbol 'emoji)
-                                      (font-spec :family font) nil 'prepend))
+             when (font-available-p font)
+             return (set-fontset-font t 'emoji (font-spec :family font) nil 'prepend))
 
     ;; Specify font for Chinese characters
-    (cl-loop for font in '("LXGW Neo Xihei" "WenQuanYi Micro Hei Mono" "LXGW WenKai Screen"
-                           "LXGW WenKai Mono" "PingFang SC" "Microsoft Yahei UI" "Simhei")
-             when (font-installed-p font)
-             return (progn
-                      (setq face-font-rescale-alist `((,font . 1.3)))
-                      (set-fontset-font t 'han (font-spec :family font))))))
+    ;; (cl-loop for font in '("LXGW Neo Xihei" "WenQuanYi Micro Hei Mono" "LXGW WenKai Screen"
+    ;;                        "LXGW WenKai Mono" "PingFang SC" "Microsoft Yahei UI" "Simhei")
+    ;;          when (font-available-p font)
+    ;;          return (progn
+    ;;                   (setq face-font-rescale-alist `((,font . 1.3)))
+    ;;                   (set-fontset-font t 'han (font-spec :family font))))
+    ))
 
 (centaur-setup-fonts)
 (add-hook 'window-setup-hook #'centaur-setup-fonts)
@@ -108,6 +108,7 @@ for the image type)."
 
 ;; Misc.
 ;; (setq confirm-kill-emacs 'y-or-n-p)
+;; (setq package-check-signature nil)
 
 ;; Enable proxy
 ;; (enable-http-proxy)
@@ -138,6 +139,12 @@ for the image type)."
 (setq default-input-method "japanese-anthy-unicode")
 
 ;; cc-mode
+(eval-after-load 'c-ts-mode
+  (add-hook 'c-ts-mode-hook
+            (lambda ()
+              (setq-local c-ts-mode-indent-style 'linux)
+              (setq-local c-ts-mode-indent-offset 4)
+              (setq-local tab-width 8))))
 (defun my-c-mode-common-hook ()
   (c-set-style "PERSONAL")
   (setq tab-width 8
